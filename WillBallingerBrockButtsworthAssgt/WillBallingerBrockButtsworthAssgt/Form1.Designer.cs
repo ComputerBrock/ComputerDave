18188028766ed5e70b1dfc4a1883278c42a0c601
Unknown <c3247137@uon.edu.au>
2017-09-25 00:26:43 +1000

    Started to Create the Form.
    Have 5 Dice with a button that makes them randomize 3 times.

--- WillBallingerBrockButtsworthAssgt/WillBallingerBrockButtsworthAssgt/Form1.Designer.cs
+++ WillBallingerBrockButtsworthAssgt/WillBallingerBrockButtsworthAssgt/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WillBallingerBrockButtsworthAssgt
 {
-    partial class Form1
+    partial class FormGameScreen
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.lblHeaderPlusMois = new System.Windows.Forms.Label();
+            this.lblGoalScore = new System.Windows.Forms.Label();
+            this.lblTargetScore = new System.Windows.Forms.Label();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.picbxDrawing = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxDrawing)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHeaderPlusMois
+            // 
+            this.lblHeaderPlusMois.AutoSize = true;
+            this.lblHeaderPlusMois.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeaderPlusMois.Location = new System.Drawing.Point(538, 9);
+            this.lblHeaderPlusMois.Name = "lblHeaderPlusMois";
+            this.lblHeaderPlusMois.Size = new System.Drawing.Size(231, 55);
+            this.lblHeaderPlusMois.TabIndex = 1;
+            this.lblHeaderPlusMois.Text = "Plus Mois";
+            this.lblHeaderPlusMois.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblGoalScore
+            // 
+            this.lblGoalScore.AutoSize = true;
+            this.lblGoalScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGoalScore.Location = new System.Drawing.Point(1004, 16);
+            this.lblGoalScore.Name = "lblGoalScore";
+            this.lblGoalScore.Size = new System.Drawing.Size(233, 46);
+            this.lblGoalScore.TabIndex = 2;
+            this.lblGoalScore.Text = "Goal Score:";
+            this.lblGoalScore.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblTargetScore
+            // 
+            this.lblTargetScore.AutoSize = true;
+            this.lblTargetScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTargetScore.Location = new System.Drawing.Point(1243, 16);
+            this.lblTargetScore.Name = "lblTargetScore";
+            this.lblTargetScore.Size = new System.Drawing.Size(86, 46);
+            this.lblTargetScore.TabIndex = 3;
+            this.lblTargetScore.Text = "200";
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(1176, 454);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(143, 43);
+            this.btnRoll.TabIndex = 4;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // picbxDrawing
+            // 
+            this.picbxDrawing.Location = new System.Drawing.Point(45, 106);
+            this.picbxDrawing.Name = "picbxDrawing";
+            this.picbxDrawing.Size = new System.Drawing.Size(1300, 120);
+            this.picbxDrawing.TabIndex = 5;
+            this.picbxDrawing.TabStop = false;
+            // 
+            // FormGameScreen
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1378, 644);
+            this.Controls.Add(this.picbxDrawing);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.lblTargetScore);
+            this.Controls.Add(this.lblGoalScore);
+            this.Controls.Add(this.lblHeaderPlusMois);
+            this.Name = "FormGameScreen";
+            this.Text = "Plus Mois";
+            ((System.ComponentModel.ISupportInitialize)(this.picbxDrawing)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblHeaderPlusMois;
+        private System.Windows.Forms.Label lblGoalScore;
+        private System.Windows.Forms.Label lblTargetScore;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.PictureBox picbxDrawing;
     }
 }
 
--- WillBallingerBrockButtsworthAssgt/WillBallingerBrockButtsworthAssgt/Form1.cs
+++ WillBallingerBrockButtsworthAssgt/WillBallingerBrockButtsworthAssgt/Form1.cs
@@ -7,14 +7,233 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+-INFT2012 - Assignment 1 - Plus Mois
+-Authors Brock Buttsworth and Ballinger.
+-Created 20/9/2017
+-Brief Description :
+    PlusMoins is a rather unforgiving and frustrating dice game. 
+    It involves five dice, and permits the player a certain amount of judgement, 
+    but luck also plays a very big part. There are two players, 
+    each of whom is trying to reach an agreed total.
+ */
 namespace WillBallingerBrockButtsworthAssgt
 {
-    public partial class Form1 : Form
+    public partial class FormGameScreen : Form
     {
-        public Form1()
+        int totalRolls = 0;
+        public FormGameScreen()
         {
             InitializeComponent();
         }
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            Graphics graPaper = picbxDrawing.CreateGraphics();
+
+            totalRolls = 0;
+            int numberOfRolls = 3;
+            //Draws First Dice
+            int tempX = 50;
+            int tempY = 50;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                drawBox(graPaper, tempX, tempY);
+                rollDice(graPaper, i, tempX, tempY);
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(1500);
+            }
+            //Draws Second Dice
+            tempX = 150;
+            tempY = 50;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                drawBox(graPaper, tempX, tempY);
+                rollDice(graPaper, i, tempX, tempY);
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(1500);
+            }
+            //Draws Third Dice
+            tempX = 250;
+            tempY = 50;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                drawBox(graPaper, tempX, tempY);
+                rollDice(graPaper, i, tempX, tempY);
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(1500);
+            }
+            //Draws Fourth Dice
+            tempX = 350;
+            tempY = 50;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                drawBox(graPaper, tempX, tempY);
+                rollDice(graPaper, i, tempX, tempY);
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(1500);
+            }
+            //Draws Fifth Dice
+            tempX = 450;
+            tempY = 50;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                drawBox(graPaper, tempX, tempY);
+                rollDice(graPaper, i, tempX, tempY);
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(1500);
+            }
+        }
+
+        private void rollDice(Graphics tempCanvas, int loopValue, int xValue, int yValue)
+        {
+            int tempX = xValue;
+            int tempY = yValue;
+            Random number = new Random();
+            int rollValue = number.Next(1, 7);  
+            //dice % Convert.ToInt32(number);
+            //txbCurrentRoll.Text = Convert.ToString(rollValue);
+            //totalRolls = totalRolls + rollValue;
+            //String appendValue = Convert.ToString(totalRolls) + "\n\r";
+            //txbTotalRoll.AppendText(String.Format("{0:d} {1:d}\n\r"), loopValue, appendValue);
+            //txbTotalRoll.Text = (appendValue);
+            //Used to Draw the Dice that was Rolled.
+            switch (rollValue)
+            {
+                case 1:
+                    roll1(tempCanvas, tempX, tempY);
+                    break;
+                case 2:
+                    roll2(tempCanvas, tempX, tempY);
+                    break;
+                case 3:
+                    roll3(tempCanvas, tempX, tempY);
+                    break;
+                case 4:
+                    roll4(tempCanvas, tempX, tempY);
+                    break;
+                case 5:
+                    roll5(tempCanvas, tempX, tempY);
+                    break;
+                case 6:
+                    roll6(tempCanvas, tempX, tempY);
+                    break;
+            }
+        }
+
+        //Used to Create a Dice Displaying a square with 1 dot
+        private void roll1(Graphics tempCanvas, int tempX, int tempY)
+        {
+            centreDot(tempCanvas, tempX, tempY);
+        }
+
+        //Used to Create a Dice Displaying a square with 2 dots
+        private void roll2(Graphics tempCanvas, int tempX, int tempY)
+        {
+            thirdDot(tempCanvas, tempX, tempY);
+            sixthDot(tempCanvas, tempX, tempY);
+        }
+
+        //Used to Create a Dice Displaying a square with 3 dots
+        private void roll3(Graphics tempCanvas, int tempX, int tempY)
+        {
+            thirdDot(tempCanvas, tempX, tempY);
+            centreDot(tempCanvas, tempX, tempY);
+            sixthDot(tempCanvas, tempX, tempY);
+        }
+
+        //Used to Create a Dice Displaying a square with 4 dots
+        private void roll4(Graphics tempCanvas, int tempX, int tempY)
+        {
+            firstDot(tempCanvas, tempX, tempY);
+            thirdDot(tempCanvas, tempX, tempY);
+            fourthDot(tempCanvas, tempX, tempY);
+            sixthDot(tempCanvas, tempX, tempY);
+        }
+
+        //Used to Create a Dice Displaying a square with 5 dots
+        private void roll5(Graphics tempCanvas, int tempX, int tempY)
+        {
+            firstDot(tempCanvas, tempX, tempY);
+            thirdDot(tempCanvas, tempX, tempY);
+            centreDot(tempCanvas, tempX, tempY);
+            fourthDot(tempCanvas, tempX, tempY);
+            sixthDot(tempCanvas, tempX, tempY);
+        }
+
+        //Used to Create a Dice Displaying a square with 6 dots
+        private void roll6(Graphics tempCanvas, int tempX, int tempY)
+        {
+            firstDot(tempCanvas, tempX, tempY);
+            secondDot(tempCanvas, tempX, tempY);
+            thirdDot(tempCanvas, tempX, tempY);
+            fourthDot(tempCanvas, tempX, tempY);
+            fifthDot(tempCanvas, tempX, tempY);
+            sixthDot(tempCanvas, tempX, tempY);
+        }
+
+        //Uses the target (X,Y) to draws a rectangle from that spot.
+        private void drawBox(Graphics tempCanvas, int targetX, int targetY)
+        {
+            tempCanvas.FillRectangle(Brushes.Black, targetX, targetY, 75, 75);
+        }
+
+        //Uses the target (X,Y) to draws a dot from that spot.
+        private void centreDot(Graphics tempCanvas, int targetX, int targetY)
+        {
+            int tempTargetX = targetX + 32;
+            int tempTargetY = targetY + 32;
+            tempCanvas.FillEllipse(Brushes.White, tempTargetX, tempTargetY, 10, 10);
+        }
+
+        //Uses the target (X,Y) to draws a dot from that spot.
+        private void firstDot(Graphics tempCanvas, int targetX, int targetY)
+        {
+            int tempTargetX = targetX + 17;
+            int tempTargetY = targetY + 17;
+            tempCanvas.FillEllipse(Brushes.White, tempTargetX, tempTargetY, 10, 10);
+        }
+
+        //Uses the target (X,Y) to draws a dot from that spot.
+        private void secondDot(Graphics tempCanvas, int targetX, int targetY)
+        {
+            int tempTargetX = targetX + 17;
+            int tempTargetY = targetY + 32;
+            tempCanvas.FillEllipse(Brushes.White, tempTargetX, tempTargetY, 10, 10);
+        }
+
+        //Uses the target (X,Y) to draws a dot from that spot.
+        private void thirdDot(Graphics tempCanvas, int targetX, int targetY)
+        {
+            int tempTargetX = targetX + 17;
+            int tempTargetY = targetY + 50;
+            tempCanvas.FillEllipse(Brushes.White, tempTargetX, tempTargetY, 10, 10);
+        }
+
+        //Uses the target (X,Y) to draws a dot from that spot.
+        private void fourthDot(Graphics tempCanvas, int targetX, int targetY)
+        {
+            int tempTargetX = targetX + 50;
+            int tempTargetY = targetY + 50;
+            tempCanvas.FillEllipse(Brushes.White, tempTargetX, tempTargetY, 10, 10);
+        }
+
+        //Uses the target (X,Y) to draws a dot from that spot.
+        private void fifthDot(Graphics tempCanvas, int targetX, int targetY)
+        {
+            int tempTargetX = targetX + 50;
+            int tempTargetY = targetY + 32;
+            tempCanvas.FillEllipse(Brushes.White, tempTargetX, tempTargetY, 10, 10);
+        }
+
+        //Uses the target (X,Y) to draws a dot from that spot.
+        private void sixthDot(Graphics tempCanvas, int targetX, int targetY)
+        {
+            int tempTargetX = targetX + 50;
+            int tempTargetY = targetY + 17;
+            tempCanvas.FillEllipse(Brushes.White, tempTargetX, tempTargetY, 10, 10);
+        }
+
+        
     }
 }
